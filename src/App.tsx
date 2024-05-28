6c58e1f8ecd42941b139fc010701fe6e9b782220
SaddamAnnais <saddamannaiss@gmail.com>
2024-05-28 13:15:57 +0700

    feat: heading and placeholder for each tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,40 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import {
-  Accordion,
-  AccordionButton,
-  AccordionIcon,
-  AccordionItem,
-  AccordionPanel,
-  Box,
   Button,
+  Card,
+  CardBody,
+  Heading,
+  Input,
+  VStack,
 } from "@chakra-ui/react";
+import { IoCheckmarkDoneSharp } from "react-icons/io5";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="px-4 py-4 max-w-screen-lg flex flex-col items-center w-full ">
+      <div className="flex flex-row w-full justify-center">
+        <IoCheckmarkDoneSharp size="3rem" color="white" />
+        <Heading as="h1" className="italic" color="white">
+          ToDos
+        </Heading>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button
-          colorScheme="blue"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </Button>
-        <Accordion>
-          <AccordionItem>
-            <h2>
-              <AccordionButton>
-                <Box as="span" flex="1" textAlign="left">
-                  Section 1 title
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel pb={4}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </AccordionPanel>
-          </AccordionItem>
-
-          <AccordionItem>
-            <h2>
-              <AccordionButton>
-                <Box as="span" flex="1" textAlign="left">
-                  Section 2 title
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel pb={4}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </AccordionPanel>
-          </AccordionItem>
-        </Accordion>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Card w="100%" mt="1rem">
+        <CardBody className="flex flex-row">
+          <Input
+            borderColor="gray"
+            placeholder="What do you need to accomplish?"
+            mr="1rem"
+          />
+          <Button colorScheme="blue">Add</Button>
+        </CardBody>
+      </Card>
+      <Card w="100%" mt="1rem">
+        <CardBody className="flex flex-row">
+          <VStack bg="white" className="w-full h-full ">
+            <p>Placeholder for each tasks</p>
+          </VStack>
+        </CardBody>
+      </Card>
+    </div>
   );
 }
 
